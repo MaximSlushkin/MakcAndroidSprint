bc39b5b47e8d6237ebfe40dfd42176c93f907a91
MakcSlushkin <slushkin.makcum@yandex.ru>
2024-12-16 17:04:57 +0300

    MAS-1-3 task is completed

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,23 @@
+package lesson_1
+
+fun main () {
+
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    println("10:55")
+
+
+
+
+
+
+
+}
