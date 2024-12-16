4a3a0230c111cd33037245fecb8a5ae84b28879b
MakcSlushkin <slushkin.makcum@yandex.ru>
2024-12-16 17:26:01 +0300

    MAS-1-3 task is completed

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Int = 27
+    val partOFDAy: Double = 0.075
+    val seconds: Int = 6480
+    val partOFYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    println(length)
+    println(age)
+    println(partOFDAy)
+    println(seconds)
+    println(partOFYear)
+    println(apogee)
+
+}
