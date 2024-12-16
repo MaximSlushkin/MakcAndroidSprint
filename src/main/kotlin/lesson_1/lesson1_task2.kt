ceb389e249d3ebaec6b8ad01a749e7f3ecdbab78
MakcSlushkin <slushkin.makcum@yandex.ru>
2024-12-16 16:33:57 +0300

    MAS-1-2 task is completed

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val completedOfOrders: Int = 75
+    val messageUser: String = "Благодарим Вас за заказ"
+    println(completedOfOrders)
+    println(messageUser)
+
+    var numberOfEmployees: Int = 2000
+//    println(numberOfEmployees)
+    numberOfEmployees = numberOfEmployees - 1
+    println(numberOfEmployees)
+}
