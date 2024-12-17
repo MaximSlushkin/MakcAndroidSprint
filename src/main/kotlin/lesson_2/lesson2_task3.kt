4a54d7358d80d5f8c3aa51411682df2b11efc57e
MakcSlushkin <slushkin.makcum@yandex.ru>
2024-12-17 22:17:24 +0300

    MAS-2-3 calculation of arrival time

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_2
+
+fun main () {
+
+    val departureHours = 9
+    val departureMinutes = 39
+    val travelMinutes = 457
+
+    val totalDepartureMinutes = departureHours * 60 + departureMinutes
+    val arrivalMinutes = totalDepartureMinutes + travelMinutes
+
+    val arrivalHours = arrivalMinutes / 60
+    val arrivalMinutesRemaining = arrivalMinutes % 60
+
+    println("Время прибытия $arrivalHours:${
+        String.format("%02d", arrivalMinutesRemaining)}")
+
+}
