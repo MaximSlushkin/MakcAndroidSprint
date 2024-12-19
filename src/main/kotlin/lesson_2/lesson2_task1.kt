b293a61b37ef69a748e7a5c77f58964f00e4fd86
MakcSlushkin <slushkin.makcum@yandex.ru>
2024-12-19 21:20:35 +0300

    KS-2-1 change in decision

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,17 +1,10 @@
 package org.example.lesson_2
 
+fun main() {
+    val evaluations = listOf(3, 4, 3, 5)
+    val averageScore = evaluations.average()
 
+    val changedAverage = String.format("%.2f", averageScore)
 
-fun main () {
-
-    val students: Int = 4
-    val markStudent1: Int = 3
-    val markStudent2: Int = 4
-    val markStudent3: Int = 3
-    val markStudent4: Int = 5
-
-    val averageScore: Double =
-    (markStudent1.toDouble() + markStudent2.toDouble() + markStudent3.toDouble() + markStudent4).toDouble() / students.toDouble()
-    println(averageScore)
-
+    println("$changedAverage")
 }
