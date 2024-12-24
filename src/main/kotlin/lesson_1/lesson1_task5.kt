bcfc156b3ecc07b3e94db2d7bb386ee002cc39b5
MakcSlushkin <slushkin.makcum@yandex.ru>
2024-12-24 13:20:19 +0300

    KS-1-5 made the number 60 a constant

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,11 @@
 package org.example.lesson_1
 fun main() {
-
-    val secondsInMinute = 60
-    val secondsInHour = secondsInMinute * 60
     var totalSeconds: Int = 6480
 
-    val hour = totalSeconds / secondsInHour
-    val minute = (totalSeconds % secondsInHour) / secondsInMinute
+    val hour = totalSeconds / secondsInMinAndHour
+    val minute = (totalSeconds % secondsInMinAndHour) / secondsInMinAndHour
 
     println("Время проведенное в космосе ${String.format("%02d", hour)}:$minute:00")
 }
+
+const val secondsInMinAndHour = 60
