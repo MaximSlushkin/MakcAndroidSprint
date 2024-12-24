36a2df0b396424e98ebd73dc92419dc36e3fff9a
MakcSlushkin <slushkin.makcum@yandex.ru>
2024-12-24 16:11:10 +0300

    KS-3-3 correction of the output println()

--- src/main/kotlin/lesson3/lesson3_task3.kt
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -1,13 +1,6 @@
 package org.example.lesson3
 
 fun main() {
-
     val number = 6
-
-    val multiplicationTable = (1..9).joinToString(separator = "\n") {
-        "$number x $it = ${number * it}"
-    }
-
-    println(multiplicationTable)
-
+    println((1..9).joinToString("\n") { "$number x $it = ${number * it}" })
 }
