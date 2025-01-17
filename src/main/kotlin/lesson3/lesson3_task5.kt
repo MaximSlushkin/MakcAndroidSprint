5fa1261b0c01c62cbb010f3307653bc19e97c36b
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-01-17 21:13:49 +0300

    KS-3-5 correcting task

--- src/main/kotlin/lesson3/lesson3_task5.kt
+++ src/main/kotlin/lesson3/lesson3_task5.kt
@@ -4,9 +4,13 @@ fun main() {
 
     val data = "D2-D4;0"
 
-    val (move, turnNumber) = data.split(";")
+    val (move, turnNumber) = data.split("-", ";")
 
-    val (fromSquare, toSquare) = move.split("-")
+    val resaultMoveSplit = move.split("-", ";")
+    println("${resaultMoveSplit::class}")
+
+    val fromSquare = resaultMoveSplit[0]
+    val toSquare = resaultMoveSplit[1]
 
     println("Откуда: $fromSquare")
     println("Куда: $toSquare")
