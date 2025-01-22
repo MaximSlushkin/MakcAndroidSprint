de5a8a8d6a2d26805165a915f5911052bbabc01d
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-01-22 19:51:51 +0300

    KS-4-5 correcting task

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,16 +1,15 @@
 package org.example.lesson_4
 
 fun main() {
+
     val hasDamage: Boolean = readLine()?.toBoolean() ?: false
     val crewCount: Int = readLine()?.toInt() ?: 0
     val foodBoxes: Int = readLine()?.toInt() ?: 0
     val favorableWeather: Boolean = readLine()?.toBoolean() ?: false
 
     val canSail = (
-            (!hasDamage && crewCount in 55..70 && foodBoxes > 50 && (favorableWeather || !favorableWeather))
-            ) || (
-            hasDamage && crewCount == 70 && favorableWeather && foodBoxes >= 50
-            )
+            (!hasDamage && crewCount in MIN_CREW_COUNT..MAX_CREW_COUNT && foodBoxes > MIN_FOOD_BOXES && favorableWeather) ||
+                    (hasDamage && crewCount == RECOMMENDED_CREW_COMPOSITION_DAMAGE && favorableWeather && foodBoxes >= MIN_FOOD_BOXES))
 
     if (canSail) {
         println("Корабль может отправиться в плавание.")
@@ -18,3 +17,8 @@ fun main() {
         println("Корабль не может отправиться в плавание.")
     }
 }
+
+const val MIN_CREW_COUNT = 55
+const val MAX_CREW_COUNT = 70
+const val MIN_FOOD_BOXES = 50
+const val RECOMMENDED_CREW_COMPOSITION_DAMAGE = 70
