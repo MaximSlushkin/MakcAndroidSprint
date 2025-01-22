388caf7a83262cf5508f751444a655ae1772e548
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-01-22 17:35:24 +0300

    KS-4-3 correcting task

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,20 @@
 package org.example.lesson_4
 
 fun main() {
-    val isSunnyWeather: Boolean = true
-    val isTentOpen: Boolean = true
-    val humidity: Int = 20
-    val currentSeason: String = "зима"
 
-    val favorableConditions = isSunnyWeather && isTentOpen && humidity == 20 && currentSeason != "зима"
+    val sunnyWeatherNow: Boolean = true
+    val tentOpenNow: Boolean = true
+    val humidityNow: Int = 20
+    val currentSeasonNow: String = "зима"
+
+    val favorableConditions = (sunnyWeatherNow && tentOpenNow && humidityNow == 20 && currentSeasonNow == "зима")
+            || (SANNY_WEATHER && TENT_OPEN && HUMADITY == 20 && CURRENT_SEASON != "зима")
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
+
 }
+
+const val SANNY_WEATHER: Boolean = true
+const val TENT_OPEN: Boolean = true
+const val HUMADITY: Int = 20
+const val CURRENT_SEASON: String = "не зима"
