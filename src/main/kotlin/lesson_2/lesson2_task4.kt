ea78ebf4e6cfd9e9104266c6714cc5fced8a17f3
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-01-23 13:28:48 +0300

    KS-2-4 correcting task

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -6,12 +6,10 @@ fun main() {
     val ironOre = 11
 
     var percentageOfProduction = 20
-    var buffMultiplier = percentageOfProduction / TOTAL_PERCENTAGE
+    var buffMultiplier = percentageOfProduction / 100
     val bonusCrystal = (crystalOre * buffMultiplier).toInt()
     val bonusIron = (ironOre * buffMultiplier).toInt()
 
     println("Бонус кристаллической руды: $bonusCrystal")
     println("Бонус железной руды: $bonusIron")
 }
-
-const val TOTAL_PERCENTAGE = 100
