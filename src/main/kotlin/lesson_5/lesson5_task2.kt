04d4eaa59632c63155faa1ca9ef70cd4992e04c8
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-01-24 20:29:09 +0300

    KS-5-2 task is completed

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_5
+
+fun main() {
+
+    println("Введите Ваш год рождения:")
+
+    val yearOfBirth = readln().toInt()
+    val currentYear = 2025
+
+    val resultText = if ((currentYear - yearOfBirth) >= 18) {
+        "Показать экран со скрытым контентом"
+    } else {
+        "Доступ заблокировать"
+    }
+
+    println(resultText)
+}
+
+const val AGE_OF_MAJORITY = 18
