c65ded1acbe50b3a4097bb07a3d2a59079f65a8e
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-02-19 21:10:50 +0300

    KS-11-4 task is solved

---
+++ src/main/kotlin/lesson_11/Lesson11_task4.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_11
+
+class Category(
+    val id: Int,
+    val nameCategory: String,
+)
+
+class Recipe(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val instructions: String,
+    val ingredients: List<Ingredient>,
+    var portions: Int,
+)
+
+class Ingredient(
+    val id: Int,
+    val name: String,
+    val quantityPerServing: String,
+)
