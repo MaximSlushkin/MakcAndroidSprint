e1b6a7f69bd3e56c4776f0c21958549570d3978b
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-03-06 18:29:50 +0300

    KS-15-1 task is corrected

--- src/main/kotlin/lesson_15/Lesson15_task1.kt
+++ src/main/kotlin/lesson_15/Lesson15_task1.kt
@@ -14,37 +14,37 @@ class CrucianCarp : Swimming {
     }
 }
 
-    class Seagull : Flight, Swimming {
+class Seagull : Flight, Swimming {
 
-        override fun fly() {
-            println("Чайка летает в небе")
-        }
-
-
-        override fun swim() {
-            println("Чайка держится на поверхности воды")
-        }
+    override fun fly() {
+        println("Чайка летает в небе")
     }
 
 
-    class Duck : Swimming, Flight {
-        override fun swim() {
-            println("Утка плавает в озере")
-        }
+    override fun swim() {
+        println("Чайка держится на поверхности воды")
+    }
+}
 
-        override fun fly() {
-            println("Утка взмывает в небо")
-        }
+
+class Duck : Swimming, Flight {
+    override fun swim() {
+        println("Утка плавает в озере")
     }
 
-    fun main() {
-        val crucianCarp = CrucianCarp()
-        val seagull = Seagull()
-        val duck = Duck()
-
-        crucianCarp.swim()
-        seagull.swim()
-        seagull.fly()
-        duck.swim()
-        duck.fly()
+    override fun fly() {
+        println("Утка взмывает в небо")
     }
+}
+
+fun main() {
+    val crucianCarp = CrucianCarp()
+    val seagull = Seagull()
+    val duck = Duck()
+
+    crucianCarp.swim()
+    seagull.swim()
+    seagull.fly()
+    duck.swim()
+    duck.fly()
+}
