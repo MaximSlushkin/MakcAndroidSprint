6bc10dc6bb835145810529ba6b0be13c770ade8e
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-03-07 17:31:36 +0300

    KS-15-2 task is corrected

--- src/main/kotlin/lesson_15/Lesson15_task2.kt
+++ src/main/kotlin/lesson_15/Lesson15_task2.kt
@@ -1,36 +1,37 @@
 package org.example.lesson_15
 
 abstract class WeatherStationStats {
-    abstract fun weatherInfo(): String
+
 }
 
 class Temperature(val tempValue: Double) : WeatherStationStats() {
-    override fun weatherInfo(): String {
+    fun weatherInfo(): String {
         return "Температура: $tempValue °C."
     }
 }
 
 class PrecipitationAmount(val precipitations: Double) : WeatherStationStats() {
-    override fun weatherInfo(): String {
+    fun weatherInfo(): String {
         return "Количество осадков: $precipitations мм."
     }
 }
 
 class WeatherServer {
-    fun weatherInfo(stats: WeatherStationStats) {
-
+    fun sendWeatherInfo(stats: WeatherStationStats) {
         println("Отправка данных о погоде на сервер...")
-        println(stats.weatherInfo())
+        when (stats) {
+            is Temperature -> println(stats.weatherInfo())
+            is PrecipitationAmount -> println(stats.weatherInfo())
+        }
     }
 }
 
 fun main() {
-
     val weatherServer = WeatherServer()
 
     val temperatureData = Temperature(-10.5)
     val precipitationData = PrecipitationAmount(11.3)
 
-    weatherServer.weatherInfo(temperatureData)
-    weatherServer.weatherInfo(precipitationData)
+    weatherServer.sendWeatherInfo(temperatureData)
+    weatherServer.sendWeatherInfo(precipitationData)
 }
