ca1822c5c8bf518f07591987ca7192003f1c0db2
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-03-07 11:16:04 +0300

    KS-16-3 task is solved

--- src/main/kotlin/lesson_16/Lesson16_task3.kt
+++ src/main/kotlin/lesson_16/Lesson16_task3.kt
@@ -1,2 +1,19 @@
 package org.example.lesson_16
 
+class User(private val username: String, private val password: String) {
+
+    fun validatePassword(inputPassword: String): Boolean {
+        return inputPassword == password
+    }
+}
+
+fun main() {
+
+    val user = User("Max", "IloveMyChild")
+
+    println("Введите ваш пароль:")
+    val inputPassword = readln()
+    val isPasswordValid = user.validatePassword(inputPassword)
+
+    println("Пароль введён верно: $isPasswordValid")
+}
