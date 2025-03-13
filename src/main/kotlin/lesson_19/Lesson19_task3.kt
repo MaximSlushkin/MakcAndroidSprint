c3405dddac4e428f7b578516981bafef104d8415
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-03-13 21:15:33 +0300

    KS-19-3 task is solved

---
+++ src/main/kotlin/lesson_19/Lesson19_task3.kt
@@ -0,0 +1,39 @@
+package org.example.lesson_19
+
+enum class SpaceshipStatus {
+    ON_THE_GROUND,
+    IN_FLIGHT,
+    UNDER_FIRE,
+}
+
+class Spaceship {
+    var status: SpaceshipStatus = SpaceshipStatus.ON_THE_GROUND
+
+    fun takeOff() {
+
+        status = SpaceshipStatus.IN_FLIGHT
+        println("Космический корабль взлетает!")
+    }
+
+    fun land() {
+
+        status = SpaceshipStatus.ON_THE_GROUND
+        println("Космический корабль приземляется!")
+    }
+
+    fun shoot() {
+        status = SpaceshipStatus.UNDER_FIRE
+        // TODO: Добавить логику выбора вида патронов для стрельбы
+        println("Космический корабль отстреливается от астероида!")
+    }
+}
+
+fun main() {
+
+    val spaceship = Spaceship()
+
+    spaceship.takeOff()
+    spaceship.shoot()
+    spaceship.land()
+
+}
