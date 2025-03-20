47a8fee64e70d132e9e99c93f508906d3bd17fc1
MakcSlushkin <slushkin.makcum@yandex.ru>
2025-03-20 15:55:23 +0300

    KS-22-4 task is corrected

--- src/main/kotlin/lesson_22/Lesson22_task5.kt
+++ src/main/kotlin/lesson_22/Lesson22_task5.kt
@@ -1,4 +1,14 @@
 package org.example.lesson_22
+//В Путеводитель для путешествующих автостопом по Галактике добавляется информация о месте или событии в галактике в виде объектов.
+//
+//Создай data class GalacticGuide cо свойствами:
+//
+//- название места или события;
+//- описание места или события;
+//- дата и время события;
+//- расстояние места или события от Земли в световых годах.
+//
+//Создай объект с информацией о звездной системе Alpha Centauri. Выведи информацию о месте, используя компонентные функции data class.
 
 import java.time.LocalDateTime
 
@@ -18,7 +28,10 @@ fun main() {
         distanceFromEarth = 4.367
     )
 
-    val (name, description, eventDateTime, distanceFromEarth) = alphaCentauri
+    val name = alphaCentauri.component1()
+    val description = alphaCentauri.component2()
+    val eventDateTime = alphaCentauri.component3()
+    val distanceFromEarth = alphaCentauri.component4()
 
     println("Название: $name")
     println("Описание: $description")
